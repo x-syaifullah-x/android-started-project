33c6d33124c3ceeb4fadb80290c3c7c73f17b5d3
x-syaifullah-x <x.syaifullah.x@gmail.com>
2021-06-08 23:03:01 +0700

    ...

--- app/src/main/java/id/xxx/example/App.kt
+++ app/src/main/java/id/xxx/example/App.kt
@@ -2,4 +2,9 @@ package id.xxx.example
 
 import android.app.Application
 
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
